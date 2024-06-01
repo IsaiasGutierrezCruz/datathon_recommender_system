8c434af7ef906257c229cf8aee3990b53b58a349
BStevOC <stevoc@outlook.es>
2024-06-01 13:49:30 -0600

    FEAT: clean users input

--- app.py
+++ app.py
@@ -10,8 +10,15 @@ load_dotenv()
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key = GOOGLE_API_KEY)
 
+# User input
+user_product_prompt = input('Search for a product')
 
+# Instructions to clean input
+instruct_clean_prompt = 'Please try to correct grammar errors and re order the sentence in a meaningful way'
+instruct_transalation_prompt = 'If the following text is not in english please translate it \n'
+
+# Call our LLM to clean our user's input
 model = GenerativeModel('gemini-pro')
-prompt = "Write a story about a brave knight."
+prompt = instruct_transalation_prompt + user_product_prompt + '\n' + instruct_clean_prompt
 response = model.generate_content(prompt)
 print(response.text)
